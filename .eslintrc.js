c4770795211a0732663464fdc535787f26f93858
Andrej Bilec <andrej.bilec@gmail.com>
2021-05-14 20:51:16 +0200

    :package: New: Standard eslint config added

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,19 @@ module.exports = {
   env: {
     browser: true,
     node: true,
+    'cypress/globals': true
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module'
   },
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+  plugins: [
+    '@typescript-eslint',
+    'cypress'
   ],
-  rules: {
-    'object-curly-spacing': ['error', 'always'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never']
-  }
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'standard'
+  ]
 }
