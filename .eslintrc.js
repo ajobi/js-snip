6bb06fa41bac84a6aa2eac9d46529c85faaac9f2
Andrej Bilec <andrej.bilec@gmail.com>
2021-05-14 21:44:53 +0200

    :ok_hand: Improve: Linting cypress config replaced by explicit plugin registration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,17 @@
 module.exports = {
   env: {
     browser: true,
-    node: true,
-    'cypress/globals': true
+    node: true
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module'
   },
-  plugins: [
-    '@typescript-eslint',
-    'cypress'
-  ],
+  plugins: ['@typescript-eslint'],
   extends: [
     'plugin:@typescript-eslint/recommended',
+    'plugin:cypress/recommended',
     'standard'
   ]
 }
