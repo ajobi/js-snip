a488e4d5fa7bee020f53e59672547a76edfeda14
Andrej Bilec <andrej.bilec@gmail.com>
2021-05-15 10:15:24 +0200

    :package: New: Validation interface added

---
+++ src/input/validateEllipsis.ts
@@ -0,0 +1,18 @@
+// import { defaultOptions } from '../defaultOptions'
+//
+// export const normalizeMaxLines = (state, maxLines) => {
+//   const parsedMaxLines = parseInt(maxLines)
+//
+//   if (!isNaN(parsedMaxLines)) {
+//     return parsedMaxLines
+//   }
+//
+//   const { maxLines: globalMaxLines } = state.options
+//   const parsedGlobalMaxLines = parseInt(globalMaxLines)
+//
+//   if (!isNaN(parsedGlobalMaxLines)) {
+//     return parsedGlobalMaxLines
+//   }
+//
+//   return defaultOptions.maxLines
+// }
