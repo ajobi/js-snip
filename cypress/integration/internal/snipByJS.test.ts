1aca38a5ee4d81f51ba7a0f8f0620701768101fe
Andrej Bilec <andrej.bilec@gmail.com>
2021-07-04 18:52:59 +0200

    :white_check_mark: Test: Complex test cases moved to snip.test
    Took 44 minutes

--- cypress/integration/internal/snipByJS.test.ts
+++ cypress/integration/internal/snipByJS.test.ts
@@ -7,116 +7,81 @@ describe('snipByJS', () => {
     cy.visit('./cypress/method.test.html')
   })
 
-  describe('Simple Scenarios', () => {
-    it('Snips on negative max lines', () => {
-      cy.get('[data-cy=paragraph]').then(($paragraph) => {
-        const paragraph = $paragraph.get()[0]
-        snipByJS(paragraph, { maxLines: -1 })
+  it('Snips on negative max lines', () => {
+    cy.get('[data-cy=paragraph]').then(($paragraph) => {
+      const paragraph = $paragraph.get()[0]
+      snipByJS(paragraph, { maxLines: -1 })
 
-        expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earum eius error expedita fuga illum iste iure minima nobis, odio praesentium quae quas ullam veniam, voluptates? Distinctio ex hic maiores obcaecati quibusdam quod repudiandae temporibus. Amet consequatur iste nisi quos! Alias atque beatae consectetur dolor doloremque earum eos expedita fugiat pariatur possimus provident quod quos, repudiandae similique sit unde ut veritatis voluptates voluptatibus voluptatum? Assumenda culpa cum eligendi, eos itaque mollitia nostrum possimus praesentium quod rerum totam.')
-      })
-    })
-
-    it('Snips on zero max lines', () => {
-      cy.get('[data-cy=paragraph]').then(($paragraph) => {
-        const paragraph = $paragraph.get()[0]
-        snipByJS(paragraph, { maxLines: 0 })
-
-        expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earum eius error expedita fuga illum iste iure minima nobis, odio praesentium quae quas ullam veniam, voluptates? Distinctio ex hic maiores obcaecati quibusdam quod repudiandae temporibus. Amet consequatur iste nisi quos! Alias atque beatae consectetur dolor doloremque earum eos expedita fugiat pariatur possimus provident quod quos, repudiandae similique sit unde ut veritatis voluptates voluptatibus voluptatum? Assumenda culpa cum eligendi, eos itaque mollitia nostrum possimus praesentium quod rerum totam.')
-      })
-    })
-
-    it('Snips on 1 max lines', () => {
-      cy.get('[data-cy=paragraph]').then(($paragraph) => {
-        const paragraph = $paragraph.get()[0]
-        snipByJS(paragraph, { maxLines: 1 })
-
-        expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earu.\u200A.\u200A.')
-      })
-    })
-
-    it('Snips on 2 max lines', () => {
-      cy.get('[data-cy=paragraph]').then(($paragraph) => {
-        const paragraph = $paragraph.get()[0]
-        snipByJS(paragraph, { maxLines: 2 })
-
-        expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earum eius error expedita fuga illum iste iure minima nobis, odio praesentium quae quas ullam ve.\u200A.\u200A.')
-      })
-    })
-
-    it('Does not snip on 10 max lines', () => {
-      cy.get('[data-cy=paragraph]').then(($paragraph) => {
-        const paragraph = $paragraph.get()[0]
-        snipByJS(paragraph, { maxLines: 10 })
-
-        expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earum eius error expedita fuga illum iste iure minima nobis, odio praesentium quae quas ullam veniam, voluptates? Distinctio ex hic maiores obcaecati quibusdam quod repudiandae temporibus. Amet consequatur iste nisi quos! Alias atque beatae consectetur dolor doloremque earum eos expedita fugiat pariatur possimus provident quod quos, repudiandae similique sit unde ut veritatis voluptates voluptatibus voluptatum? Assumenda culpa cum eligendi, eos itaque mollitia nostrum possimus praesentium quod rerum totam.')
-      })
-    })
-
-    it('Maintains the original style attributes', () => {
-      cy.get('[data-cy=paragraph]').then(($paragraph) => {
-        const paragraph = $paragraph.get()[0]
-        const originalColor = paragraph.style.color
-
-        snipByJS(paragraph, { maxLines: 2 })
-
-        expect(paragraph.style.color).to.equal(originalColor)
-      })
-    })
-
-    it('Applies custom ellipsis properly', () => {
-      cy.get('[data-cy=paragraph]').then(($paragraph) => {
-        const paragraph = $paragraph.get()[0]
-        snipByJS(paragraph, { maxLines: 1, ellipsis: 'AAA' })
-
-        expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur eaAAA')
-      })
-    })
-
-    it('Works with custom separators', () => {
-      cy.get('[data-cy=paragraph]').then(($paragraph) => {
-        const paragraph = $paragraph.get()[0]
-        snipByJS(paragraph, { maxLines: 1, separators: [''] })
-
-        expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earu.\u200A.\u200A.')
-      })
+      expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earum eius error expedita fuga illum iste iure minima nobis, odio praesentium quae quas ullam veniam, voluptates? Distinctio ex hic maiores obcaecati quibusdam quod repudiandae temporibus. Amet consequatur iste nisi quos! Alias atque beatae consectetur dolor doloremque earum eos expedita fugiat pariatur possimus provident quod quos, repudiandae similique sit unde ut veritatis voluptates voluptatibus voluptatum? Assumenda culpa cum eligendi, eos itaque mollitia nostrum possimus praesentium quod rerum totam.')
     })
   })
 
-  describe('Complex Scenarios', () => {
-    it('Is able to increase / decrease max lines', () => {
-      cy.get('[data-cy=paragraph]').then(($paragraph) => {
-        const paragraph = $paragraph.get()[0]
+  it('Snips on zero max lines', () => {
+    cy.get('[data-cy=paragraph]').then(($paragraph) => {
+      const paragraph = $paragraph.get()[0]
+      snipByJS(paragraph, { maxLines: 0 })
 
-        snipByJS(paragraph, { maxLines: 1 })
-        expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earu.\u200A.\u200A.')
-
-        snipByJS(paragraph, { maxLines: 2 })
-        expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earum eius error expedita fuga illum iste iure minima nobis, odio praesentium quae quas ullam ve.\u200A.\u200A.')
-
-        snipByJS(paragraph, { maxLines: 1 })
-        expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earu.\u200A.\u200A.')
-      })
+      expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earum eius error expedita fuga illum iste iure minima nobis, odio praesentium quae quas ullam veniam, voluptates? Distinctio ex hic maiores obcaecati quibusdam quod repudiandae temporibus. Amet consequatur iste nisi quos! Alias atque beatae consectetur dolor doloremque earum eos expedita fugiat pariatur possimus provident quod quos, repudiandae similique sit unde ut veritatis voluptates voluptatibus voluptatum? Assumenda culpa cum eligendi, eos itaque mollitia nostrum possimus praesentium quod rerum totam.')
     })
+  })
 
-    it('Is able to change text ', () => {
-      // cy.get('[data-cy=paragraph]').then(($paragraph) => {
-      // const paragraph = $paragraph.get()[0]
-      //
-      // snipByJS(paragraph, { maxLines: 1 })
-      // expect(paragraph.innerText).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earu.\u200A.\u200A.')
-      //
-      // const newText = 'This is some custom text that is only useful for this test case. The text should now be visible on the page and properly clamped to exactly two lines. Not more, not less. This text is completely useless for the world.'
-      // paragraph.innerText = newText
-      //
-      // snipByJS(paragraph, { maxLines: 1 })
-      // expect(paragraph.innerText).to.equal('This is some custom text that is only useful for this test case. The text should now be visible on the page and properly.\u200A.\u200A.')
-      // expect(paragraph.dataset.fullText).to.equal(newText)
-      // })
-    })
+  it('Snips on 1 max lines', () => {
+    cy.get('[data-cy=paragraph]').then(($paragraph) => {
+      const paragraph = $paragraph.get()[0]
+      snipByJS(paragraph, { maxLines: 1 })
 
-    it('Is able to work with nested elements', () => {
-      // TODO: Add the test
+      expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earu.\u200A.\u200A.')
     })
+  })
+
+  it('Snips on 2 max lines', () => {
+    cy.get('[data-cy=paragraph]').then(($paragraph) => {
+      const paragraph = $paragraph.get()[0]
+      snipByJS(paragraph, { maxLines: 2 })
+
+      expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earum eius error expedita fuga illum iste iure minima nobis, odio praesentium quae quas ullam ve.\u200A.\u200A.')
+    })
+  })
+
+  it('Does not snip on 10 max lines', () => {
+    cy.get('[data-cy=paragraph]').then(($paragraph) => {
+      const paragraph = $paragraph.get()[0]
+      snipByJS(paragraph, { maxLines: 10 })
+
+      expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earum eius error expedita fuga illum iste iure minima nobis, odio praesentium quae quas ullam veniam, voluptates? Distinctio ex hic maiores obcaecati quibusdam quod repudiandae temporibus. Amet consequatur iste nisi quos! Alias atque beatae consectetur dolor doloremque earum eos expedita fugiat pariatur possimus provident quod quos, repudiandae similique sit unde ut veritatis voluptates voluptatibus voluptatum? Assumenda culpa cum eligendi, eos itaque mollitia nostrum possimus praesentium quod rerum totam.')
+    })
+  })
+
+  it('Maintains the original style attributes', () => {
+    cy.get('[data-cy=paragraph]').then(($paragraph) => {
+      const paragraph = $paragraph.get()[0]
+      const originalColor = paragraph.style.color
+
+      snipByJS(paragraph, { maxLines: 2 })
+
+      expect(paragraph.style.color).to.equal(originalColor)
+    })
+  })
+
+  it('Applies custom ellipsis properly', () => {
+    cy.get('[data-cy=paragraph]').then(($paragraph) => {
+      const paragraph = $paragraph.get()[0]
+      snipByJS(paragraph, { maxLines: 1, ellipsis: 'AAA' })
+
+      expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur eaAAA')
+    })
+  })
+
+  it('Works with custom separators', () => {
+    cy.get('[data-cy=paragraph]').then(($paragraph) => {
+      const paragraph = $paragraph.get()[0]
+      snipByJS(paragraph, { maxLines: 1, separators: [''] })
+
+      expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earu.\u200A.\u200A.')
+    })
+  })
+
+  it('Is able to work with nested elements', () => {
+    // TODO: Implement the feature
   })
 })
