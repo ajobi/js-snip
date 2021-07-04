ece1a9e22d8e8160b10161ddc05371251a59cea1
Andrej Bilec <andrej.bilec@gmail.com>
2021-07-04 18:52:59 +0200

    :white_check_mark: Test: Complex test cases moved to snip.test
    Took 44 minutes

--- cypress/integration/distribution/snip.test.ts
+++ cypress/integration/distribution/snip.test.ts
@@ -37,7 +37,71 @@ describe('snip', () => {
     })
   })
 
-  it('Is able to switch snipping methods', () => {
-    // TODO: Add the test
+  describe('Complex Scenarios', () => {
+    it('Is able to increase / decrease max lines (CSS)', () => {
+      cy.get('[data-cy=paragraph]').then(($paragraph) => {
+        const paragraph = $paragraph.get()[0]
+
+        snip(paragraph, { maxLines: 1, method: 'css' })
+        expect(getLines(paragraph)).to.equal(1)
+
+        snip(paragraph, { maxLines: 2, method: 'css' })
+        expect(getLines(paragraph)).to.equal(2)
+
+        snip(paragraph, { maxLines: 1, method: 'css' })
+        expect(getLines(paragraph)).to.equal(1)
+      })
+    })
+
+    it('Is able to change text (CSS)', () => {
+      cy.get('[data-cy=paragraph]').then(($paragraph) => {
+        const paragraph = $paragraph.get()[0]
+
+        snip(paragraph, { maxLines: 2, method: 'css' })
+        expect(getLines(paragraph)).to.equal(2)
+
+        const newText = 'This is some custom text that is only useful for this test case. The text should now be visible on the page and properly clamped to exactly two lines. Not more, not less. This text is completely useless for the world.'
+        paragraph.textContent = newText
+
+        snip(paragraph, { maxLines: 2, method: 'css' })
+        expect(getLines(paragraph)).to.equal(2)
+        expect(paragraph.dataset.fullText).to.equal(newText)
+      })
+    })
+
+    it('Is able to increase / decrease max lines (JS)', () => {
+      cy.get('[data-cy=paragraph]').then(($paragraph) => {
+        const paragraph = $paragraph.get()[0]
+
+        snip(paragraph, { maxLines: 1, method: 'js' })
+        expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earu.\u200A.\u200A.')
+
+        snip(paragraph, { maxLines: 2, method: 'js' })
+        expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earum eius error expedita fuga illum iste iure minima nobis, odio praesentium quae quas ullam ve.\u200A.\u200A.')
+
+        snip(paragraph, { maxLines: 1, method: 'js' })
+        expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earu.\u200A.\u200A.')
+      })
+    })
+
+    it('Is able to change text (JS)', () => {
+      cy.get('[data-cy=paragraph]').then(($paragraph) => {
+        const paragraph = $paragraph.get()[0]
+
+        snip(paragraph, { maxLines: 1, method: 'js' })
+        expect(paragraph.textContent).to.equal('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias animi aut, consectetur earu.\u200A.\u200A.')
+
+        const newText = 'This is some custom text that is only useful for this test case. The text should now be visible on the page and properly clamped to exactly two lines. Not more, not less. This text is completely useless for the world.'
+        paragraph.textContent = newText
+
+        snip(paragraph, { maxLines: 1, method: 'js' })
+        expect(paragraph.textContent).to.equal('This is some custom text that is only useful for this test case. The text should now be visible on the page and properly.\u200A.\u200A.')
+        expect(paragraph.dataset.fullText).to.equal(newText)
+      })
+    })
+
+    it('Is able to switch snipping methods', () => {
+      // TODO: Add the test
+    })
   })
 })
--- cypress/integration/internal/snipByCSS.test.ts
+++ cypress/integration/internal/snipByCSS.test.ts
@@ -6,104 +6,69 @@ describe('snipByCSS', () => {
     cy.visit('./cypress/method.test.html')
   })
 
-  describe('Simple Scenarios', () => {
-    it('Snips on negative max lines', () => {
-      cy.get('[data-cy=paragraph]').then(($paragraph) => {
-        const paragraph = $paragraph.get()[0]
-        const oldLines = getLines(paragraph)
+  it('Snips on negative max lines', () => {
+    cy.get('[data-cy=paragraph]').then(($paragraph) => {
+      const paragraph = $paragraph.get()[0]
+      const oldLines = getLines(paragraph)
 
-        snipByCSS(paragraph, { maxLines: -1 })
+      snipByCSS(paragraph, { maxLines: -1 })
 
-        expect(getLines(paragraph)).to.equal(oldLines)
-      })
-    })
-
-    it('Snips on zero max lines', () => {
-      cy.get('[data-cy=paragraph]').then(($paragraph) => {
-        const paragraph = $paragraph.get()[0]
-        const oldLines = getLines(paragraph)
-
-        snipByCSS(paragraph, { maxLines: 0 })
-
-        expect(getLines(paragraph)).to.equal(oldLines)
-      })
-    })
-
-    it('Snips on 1 max lines', () => {
-      cy.get('[data-cy=paragraph]').then(($paragraph) => {
-        const paragraph = $paragraph.get()[0]
-        snipByCSS(paragraph, { maxLines: 1 })
-
-        expect(getLines(paragraph)).to.equal(1)
-      })
-    })
-
-    it('Snips on 2 max lines', () => {
-      cy.get('[data-cy=paragraph]').then(($paragraph) => {
-        const paragraph = $paragraph.get()[0]
-        snipByCSS(paragraph, { maxLines: 2 })
-
-        expect(getLines(paragraph)).to.equal(2)
-      })
-    })
-
-    it('Does not snip on 10 max lines', () => {
-      cy.get('[data-cy=paragraph]').then(($paragraph) => {
-        const paragraph = $paragraph.get()[0]
-        const oldLines = getLines(paragraph)
-
-        snipByCSS(paragraph, { maxLines: 10 })
-
-        expect(getLines(paragraph)).to.equal(oldLines)
-      })
-    })
-
-    it('Maintains the original style attributes', () => {
-      cy.get('[data-cy=paragraph]').then(($paragraph) => {
-        const paragraph = $paragraph.get()[0]
-        const originalColor = paragraph.style.color
-
-        snipByCSS(paragraph, { maxLines: 2 })
-
-        expect(paragraph.style.color).to.equal(originalColor)
-      })
+      expect(getLines(paragraph)).to.equal(oldLines)
     })
   })
 
-  describe('Complex Scenarios', () => {
-    it('Is able to increase / decrease max lines', () => {
-      cy.get('[data-cy=paragraph]').then(($paragraph) => {
-        const paragraph = $paragraph.get()[0]
+  it('Snips on zero max lines', () => {
+    cy.get('[data-cy=paragraph]').then(($paragraph) => {
+      const paragraph = $paragraph.get()[0]
+      const oldLines = getLines(paragraph)
 
-        snipByCSS(paragraph, { maxLines: 1 })
-        expect(getLines(paragraph)).to.equal(1)
+      snipByCSS(paragraph, { maxLines: 0 })
 
-        snipByCSS(paragraph, { maxLines: 2 })
-        expect(getLines(paragraph)).to.equal(2)
-
-        snipByCSS(paragraph, { maxLines: 1 })
-        expect(getLines(paragraph)).to.equal(1)
-      })
+      expect(getLines(paragraph)).to.equal(oldLines)
     })
+  })
 
-    it('Is able to change text ', () => {
-      cy.get('[data-cy=paragraph]').then(($paragraph) => {
-        const paragraph = $paragraph.get()[0]
+  it('Snips on 1 max lines', () => {
+    cy.get('[data-cy=paragraph]').then(($paragraph) => {
+      const paragraph = $paragraph.get()[0]
+      snipByCSS(paragraph, { maxLines: 1 })
 
-        snipByCSS(paragraph, { maxLines: 2 })
-        expect(getLines(paragraph)).to.equal(2)
-
-        const newText = 'This is some custom text that is only useful for this test case. The text should now be visible on the page and properly clamped to exactly two lines. Not more, not less. This text is completely useless for the world.'
-        paragraph.textContent = newText
-
-        snipByCSS(paragraph, { maxLines: 2 })
-        expect(getLines(paragraph)).to.equal(2)
-        expect(paragraph.dataset.fullText).to.equal(newText)
-      })
+      expect(getLines(paragraph)).to.equal(1)
     })
+  })
 
-    it('Is able to work with nested elements', () => {
-      // TODO: Add the test
+  it('Snips on 2 max lines', () => {
+    cy.get('[data-cy=paragraph]').then(($paragraph) => {
+      const paragraph = $paragraph.get()[0]
+      snipByCSS(paragraph, { maxLines: 2 })
+
+      expect(getLines(paragraph)).to.equal(2)
     })
+  })
+
+  it('Does not snip on 10 max lines', () => {
+    cy.get('[data-cy=paragraph]').then(($paragraph) => {
+      const paragraph = $paragraph.get()[0]
+      const oldLines = getLines(paragraph)
+
+      snipByCSS(paragraph, { maxLines: 10 })
+
+      expect(getLines(paragraph)).to.equal(oldLines)
+    })
+  })
+
+  it('Maintains the original style attributes', () => {
+    cy.get('[data-cy=paragraph]').then(($paragraph) => {
+      const paragraph = $paragraph.get()[0]
+      const originalColor = paragraph.style.color
+
+      snipByCSS(paragraph, { maxLines: 2 })
+
+      expect(paragraph.style.color).to.equal(originalColor)
+    })
+  })
+
+  it('Is able to work with nested elements', () => {
+    // TODO: Implement the feature
   })
 })
