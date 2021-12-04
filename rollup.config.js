d80c25d2e7d9dab00a261f0e200881d1fc9356ff
Andrej Bilec <andrej.bilec@gmail.com>
2021-12-04 12:04:56 +0100

    :package: New: Rollup dev server
    Took 2 minutes

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve'
 import { babel } from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 import dts from 'rollup-plugin-dts'
+import serve from 'rollup-plugin-serve'
 import pkg from './package.json'
 
 const input = 'src/index.ts'
@@ -9,7 +10,8 @@ const input = 'src/index.ts'
 const plugins = [
   nodeResolve({ extensions: ['.ts'] }),
   babel({ babelHelpers: 'bundled', extensions: ['.ts'] }),
-  terser()
+  terser(),
+  ...(process.env.NODE_ENV === 'development' ? [serve({ port: 3000, contentBase: 'docs', open: true })] : [])
 ]
 
 export default [
