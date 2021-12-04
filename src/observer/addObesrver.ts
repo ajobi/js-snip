d810208dbdbeaab9d5aea70628b9cf4d46708918
Andrej Bilec <andrej.bilec@gmail.com>
2021-12-04 17:35:21 +0100

    :ok_hand: Improve: Local snipText function added
    Took 5 minutes

--- src/observer/addObesrver.ts
+++ src/observer/addObesrver.ts
@@ -1,11 +1,11 @@
-import { snip } from '../snip'
+import { snipText } from '../snip/snip'
 
 export const addObserver = (el: HTMLElement): void => {
   const elState = window.__JsSnipState.get(el)
 
   const observer = elState.observer || new ResizeObserver(() => {
     if (el.clientWidth !== elState.prevWidth || el.clientHeight !== elState.prevHeight) {
-      snip(el, elState)
+      snipText(el)
     }
   })
 
