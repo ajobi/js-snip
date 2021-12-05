b3eaf1b3e03db0b1d04c77eb1fd44b624b6fa2b8
Andrej Bilec <andrej.bilec@gmail.com>
2021-12-05 14:02:57 +0100

    :ok_hand: Improve: Some observer tests fixed
    Took 10 minutes

--- cypress/integration/destroyObserver.test.ts
+++ cypress/integration/destroyObserver.test.ts
@@ -1,25 +1,23 @@
-// import { addObserver, destroyObserver } from '../../instrumented/observer'
-// import { defaultOptions } from '../../instrumented/defaultOptions'
-//
-// describe('addObserver', () => {
-//   beforeEach(() => {
-//     cy.visit('./cypress/tests/paragraph-single.html')
-//   })
-//
-//   it('Removes the observer from the element state', () => {
-//     cy.get('[data-cy=paragraph]').then(($paragraph) => {
-//       const paragraph = $paragraph.get()[0]
-//       const elementMap = new WeakMap()
-//       elementMap.set(paragraph, {})
-//
-//       const snipText = cy.stub()
-//       const state = { elementMap, options: defaultOptions }
-//
-//       addObserver(state, snipText, paragraph)
-//       destroyObserver(state, paragraph)
-//
-//       expect(state.elementMap.get(paragraph).observer).eq(undefined)
-//       expect(state.elementMap.get(paragraph).observer).eq(undefined)
-//     })
-//   })
-// })
+import { addObserver, destroyObserver } from '../../instrumented/observer'
+import { getState, setState } from '../../instrumented/utils'
+import { getMockState } from './snipByCSS.test'
+
+describe('addObserver', () => {
+  beforeEach(() => {
+    cy.visit('./cypress/tests/paragraph-single.html')
+  })
+
+  it('Removes the observer from the element state', () => {
+    cy.get('[data-cy=paragraph]').then(($paragraph) => {
+      const paragraph = $paragraph.get()[0]
+
+      setState(paragraph, getMockState(paragraph, {}))
+
+      addObserver(paragraph)
+      destroyObserver(paragraph)
+
+      expect(getState(paragraph).observer).eq(undefined)
+      expect(getState(paragraph).observer).eq(undefined)
+    })
+  })
+})
