3d86c900c92f552f0e449a9b73edd3447c0c82f1
Andrej Bilec <andrej.bilec@gmail.com>
2021-12-05 10:18:10 +0100

    :ok_hand: Improve: Parse ellipsis improved for non number and string types
    Took 11 minutes

--- cypress/integration/parseEllipsis.test.ts
+++ cypress/integration/parseEllipsis.test.ts
@@ -1,5 +1,19 @@
+import { parseEllipsis } from '../../instrumented/input'
+import { defaultOptions } from '../../instrumented/defaultOptions'
+
 describe('parseEllipsis', () => {
-  it('dummy test', () => {
-    expect(true).to.equal(true)
+  it('returns original string values', () => {
+    expect(parseEllipsis('test')).to.equal('test')
+  })
+
+  it('returns parsed int values', () => {
+    expect(parseEllipsis(123)).to.equal('123')
+  })
+
+  it('returns default ellipsis on non-parseable value', () => {
+    expect(parseEllipsis(undefined)).to.equal(defaultOptions.ellipsis)
+    expect(parseEllipsis(null)).to.equal(defaultOptions.ellipsis)
+    expect(parseEllipsis({})).to.equal(defaultOptions.ellipsis)
+    expect(parseEllipsis([])).to.equal(defaultOptions.ellipsis)
   })
 })
--- src/input/parseEllipsis.ts
+++ src/input/parseEllipsis.ts
@@ -1,10 +1,12 @@
 import { defaultOptions } from '../defaultOptions'
 
 export const parseEllipsis = (ellipsis: unknown): string => {
-  ellipsis = ellipsis.toString()
-
   if (typeof ellipsis === 'string') {
     return ellipsis
+  }
+
+  if (typeof ellipsis === 'number') {
+    return ellipsis.toString()
   }
 
   return defaultOptions.ellipsis
