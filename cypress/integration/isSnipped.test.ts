8236a62cb330da1af34006efdd6a2f1c99e1b801
Andrej Bilec <andrej.bilec@gmail.com>
2022-02-09 19:19:41 +0100

    :package: New: Utility for snipped state checks added (js method)

--- cypress/integration/isSnipped.test.ts
+++ cypress/integration/isSnipped.test.ts
@@ -1,4 +1,5 @@
 import { isSnipped } from '../../instrumented/utils'
+import { snip } from '../../instrumented'
 
 describe('isSnipped', () => {
   beforeEach(() => {
@@ -9,6 +10,26 @@ describe('isSnipped', () => {
     cy.get('[data-cy=paragraph]').then(($paragraph) => {
       const paragraph = $paragraph.get()[0]
       expect(isSnipped(paragraph)).to.equal(false)
+    })
+  })
+
+  it('Returns true on css snipped elements', () => {
+    cy.get('[data-cy=paragraph]').then(($paragraph) => {
+      const paragraph = $paragraph.get()[0]
+
+      snip(paragraph, { lines: 2, method: 'css' })
+
+      expect(isSnipped(paragraph)).to.equal(true)
+    })
+  })
+
+  it('Returns true on js snipped elements', () => {
+    cy.get('[data-cy=paragraph]').then(($paragraph) => {
+      const paragraph = $paragraph.get()[0]
+
+      snip(paragraph, { lines: 2, method: 'js' })
+
+      expect(isSnipped(paragraph)).to.equal(true)
     })
   })
 })
--- src/utils/isSnipped.ts
+++ src/utils/isSnipped.ts
@@ -1,3 +1,15 @@
+import { getState } from './elementState'
+
 export const isSnipped = (element: HTMLElement): boolean => {
+  const elState = getState(element)
+
+  if (elState?.method === 'js') {
+    return elState.fullText === element.textContent
+  }
+
+  if (elState?.method === 'css') {
+    return false
+  }
+
   return false
 }
