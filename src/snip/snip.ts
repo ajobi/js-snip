7c02c4d5bc3b904b6ed1eadfd1db0fb0423a2e29
Andrej Bilec <andrej.bilec@gmail.com>
2022-02-13 15:50:46 +0100

    :ok_hand: Refactor: Naming of the state and element references unified

--- src/snip/snip.ts
+++ src/snip/snip.ts
@@ -7,13 +7,13 @@ import { parseOptions } from '../input'
 // TODO: add the tests for the posibility to change the onStateChanged callback
 export const snip: Snip = (element, options, onStateChanged) => {
   const isFirstSnip = !hasState(element)
-  const elState = getState(element)
+  const state = getState(element)
 
   setState(element, {
-    ...elState,
+    ...state,
     ...parseOptions(options),
-    hasEllipsis: isFirstSnip ? false : elState?.hasEllipsis,
-    fullText: isFirstSnip ? element.textContent : elState?.fullText,
+    hasEllipsis: isFirstSnip ? false : state?.hasEllipsis,
+    fullText: isFirstSnip ? element.textContent : state?.fullText,
   })
 
   const snip = () => {
