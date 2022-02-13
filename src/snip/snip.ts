f4bbc642073c874133d05f33b2879940aec4ec13
Andrej Bilec <andrej.bilec@gmail.com>
2022-02-13 15:23:31 +0100

    :package: New: Using the onStateChanged callback

--- src/snip/snip.ts
+++ src/snip/snip.ts
@@ -1,10 +1,10 @@
 import { Snip } from '../types'
 import { addObserver } from '../observer'
-import { getState, hasState, setState } from '../utils'
+import { getState, hasState, setState, getPublicState } from '../utils'
 import { applySnipping } from '../methods'
 import { parseOptions } from '../input'
 
-export const snip: Snip = (element, options) => {
+export const snip: Snip = (element, options, onStateChanged) => {
   const isFirstSnip = !hasState(element)
   const elState = getState(element)
 
@@ -15,7 +15,9 @@ export const snip: Snip = (element, options) => {
   })
 
   const onElementResize = () => {
+    const oldState = getPublicState(element)
     applySnipping(element)
+    onStateChanged(oldState, getPublicState(element))
   }
 
   isFirstSnip && typeof ResizeObserver !== 'undefined' ? addObserver(element, onElementResize) : applySnipping(element)
