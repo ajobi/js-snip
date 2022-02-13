4283e8faf1d79e29cf487f568158121ef174c5bc
Andrej Bilec <andrej.bilec@gmail.com>
2022-02-13 13:45:48 +0100

    :package: New: Public SnipState interface added

--- src/types.ts
+++ src/types.ts
@@ -7,8 +7,16 @@ export interface SnipOptions {
   midWord: boolean
 }
 
+export interface SnipState {
+  hasEllipsis: boolean
+}
+
 export interface Snip {
-  (element: HTMLElement, options: Partial<SnipOptions>): void
+  (
+    element: HTMLElement,
+    options: Partial<SnipOptions>,
+    onStateChanged: (newState: SnipState, oldState: SnipState) => void
+  ): void
 }
 
 export interface Unsnip {
