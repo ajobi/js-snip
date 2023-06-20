82f2db328ff9fbe5421a6114d7870f3bccc95ab6
Ajo Bilec <andrej.bilec@gmail.com>
2023-06-20 16:23:06 +0200

    :book: Doc: Documents todos

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-// TODO: replace travis
+/*
+ * TODOs:
+ * - remove the issues shield
+ * - migrate from travis to GitHub Actions
+ * - fix resize observer issues in tests
+ * */
 export * from './types'
 export { snip, unsnip } from './snip'
 export { getLines, supportsCSSMode } from './utils'
